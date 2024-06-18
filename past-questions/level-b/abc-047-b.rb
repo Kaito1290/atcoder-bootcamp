d300f9bd1810a592f25544e6f1676cceba66d61f
Kaito1290 <sirobu9963@gmail.com>
2024-06-18 21:44:36 +0900

    abc-047-b.rbを修正

--- past-questions/level-b/abc-047-b.rb
+++ past-questions/level-b/abc-047-b.rb
@@ -3,7 +3,7 @@
 # x座標、y座標、点の数
 width, hight, num = gets.split.map(&:to_i)
 # 2次元配列
-array = Array.new(num) {gets.split.map(&:to_i)}
+array_xya = Array.new(num) {gets.split.map(&:to_i)}
 
 x_right = width
 x_left = 0
@@ -11,20 +11,20 @@ y_top = hight
 y_botoom = 0
 
 # 処理
-array.each do |arr|
-  case arr[2]
+array_xya.each do |x,y,a|
+  case a
   when 1
     # a = 1のとき => x座標より左側を塗りつぶす
-    x_left = arr[0]
+    x_left = [x_left,x].max
   when 2
     # a = 2のとき => x座標より右側を塗りつぶす
-    x_right = arr[0]
+    x_right = [x_right,x].min
   when 3
     # a = 3のとき => y座標より下側を塗りつぶす
-    y_botoom = arr[1]
+    y_botoom = [y_botoom,y].max
   when 4
     # a = 4のとき => y座標より上側を塗りつぶす
-    y_top = arr[1]
+    y_top = [y_top,y].min
   end
 end
 
